28fe67db19231fe355f351cbfb00b7a0a6ba4429
MehdiLamrani <mehdi.lamrani@gmail.com>
2020-09-17 22:24:15 +0200

    application code

--- application/mysql_test.py
+++ application/mysql_test.py
@@ -9,15 +9,20 @@ SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://user:password@host/dbname'
 engine = sqlalchemy.create_engine(SQLALCHEMY_DATABASE_URI, echo=True)
 print(engine.table_names())
 
+###################################################################################################################################################
+#                  CREATE A MYSQL/MARIADB DATABASE
+
 # MAC :
 # brew install mariadb
 # sudo mysql -u root
 # mysql.server start
 # https://phoenixnap.com/kb/how-to-create-mariadb-user-grant-privileges#:~:text=To%20create%20a%20new%20MariaDB,to%20a%20local%20MySQL%20server.
 
-# WIN
-#https://downloads.mariadb.org/interstitial/mariadb-10.5.5/winx64-packages/mariadb-10.5.5-winx64.msi/from/http%3A//ams2.mirrors.digitalocean.com/mariadb/
+# WINDOWS
+# https://downloads.mariadb.org/interstitial/mariadb-10.5.5/winx64-packages/mariadb-10.5.5-winx64.msi/from/http%3A//ams2.mirrors.digitalocean.com/mariadb/
 
-
-
-
+###################################################################################################################################################
+#                  CONNECT FROM PYCHARM
+#  View > Tool Window > Database
+# https://www.jetbrains.com/help/pycharm/connecting-to-a-database.html#connect-to-mysql-database
+###################################################################################################################################################
--- application/sqlite_test.py
+++ application/sqlite_test.py
@@ -8,3 +8,8 @@ mycur.execute("SELECT name FROM users ")
 users=(mycur.fetchall())
 print(available_tables)
 print(users)
+
+###################################################################################################################################################
+# CREATE A SQLite DB in PYCHARM
+# https://phoenixnap.com/kb/how-to-create-mariadb-user-grant-privileges#:~:text=To%20create%20a%20new%20MariaDB,to%20a%20local%20MySQL%20server.
+###################################################################################################################################################
